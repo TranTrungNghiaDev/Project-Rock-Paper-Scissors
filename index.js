ef55e539b9822a5c16b276e07f9b4dd3e7df95bb
TranTrungNghiaDev <trantrungnghiadev1998@gmail.com>
2024-06-23 14:27:27 +0700

    Add UI for rock-paper-scissor game
    I add images to display player choice and computer choice
    Display result in browser not in console

--- index.js
+++ index.js
@@ -13,46 +13,83 @@ function getComputerChoice() {
 
     if (randomNumber < 3) {
         answer = "Rock";
+        displayComputerChoice("Rock");
     }
     else if (randomNumber >= 3 && randomNumber <= 6) {
         answer = "Scissor";
+        displayComputerChoice("Scissor");
     }
     else {
+        displayComputerChoice("Paper");
         answer = "Paper";
     }
 
     return answer;
 }
 
-// -- Start function getplayerChoice
-// Show the message to ask user input their choice
-// Change user's input to lowercase to easy compare
-// If user's input correct then isAnswerCorrect = true
-// Return their answer
-// While isAnswerCorrect = false
-// Show message : "The input is wrong! Try again" then make user input their answer again
-// Do it until isAnswerCorrect = true
-// -- End function getplayerChoice
-function getPlayerChoice() {
-    let answer = prompt("Input your choice (Rock/Paper/Scissor) here :").toLowerCase();
-    answer = answer.replace(answer[0], answer[0].toUpperCase());
-    let isAnswerCorrect = false;
-
-    do {
-        if (answer == "Rock" || answer == "Paper" || answer == "Scissor") {
-            isAnswerCorrect = true;
-        }
-        else {
-            answer = prompt("Your input is not correct! Try again.").toLowerCase();
-            answer = answer.replace(answer[0], answer[0].toUpperCase());
-        }
-    }
-
-    while (isAnswerCorrect == false)
-
-    return answer;
+function displayComputerChoice(computerChoice) {
+    computerChoiceImg.setAttribute("src", `./images/${computerChoice}.png`);
 }
 
+function displayPlayerChoice(playerChoice) {
+    playerChoiceImg.setAttribute("src", `./images/${playerChoice}.png`);
+}
+
+// Create variables to keep track human's score and computer's score
+let playerScore = 0;
+let computerScore = 0;
+
+// -- Start Function playRound
+//Get human choice and computer choice
+// If human choice === computer choice so the result is equal
+// End If
+// Else 
+// If human choice is Rock, check computer choice is paper/scissor
+// If computer choice is paper then result is Player Lose, computerScore++
+// If computer choice is scissor then result is Player Win, playerScore++
+// Another human/computer choices do the same
+// End Else
+// Go to the next round
+// -- End Function playRound
+let playRound = function (playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        paraResult.textContent = "The result is equal!";
+    }
+    else {
+        if (playerChoice === "Rock") {
+            if (computerChoice === "Paper") {
+                computerScore++;
+                updateResult(`You lose! ${computerChoice} beats ${playerChoice}`);
+            }
+            else {
+                playerScore++;
+                updateResult(`You Win! ${playerChoice} beats ${computerChoice}`);
+            }
+        }
+
+        else if (playerChoice === "Paper") {
+            if (computerChoice === "Scissor") {
+                computerScore++;
+                updateResult(`You lose! ${computerChoice} beats ${playerChoice}`);
+            }
+            else {
+                playerScore++;
+                updateResult(`You Win! ${playerChoice} beats ${computerChoice}`);
+            }
+        }
+
+        else {
+            if (computerChoice === "Rock") {
+                computerScore++;
+                updateResult(`You lose! ${computerChoice} beats ${playerChoice}`);
+            }
+            else {
+                playerScore++;
+                updateResult(`You Win! ${playerChoice} beats ${computerChoice}`);
+            }
+        }
+    }
+}
 
 
 // Start function playGame
@@ -61,79 +98,84 @@ function getPlayerChoice() {
 // If player score > computer score. Show Message Player win
 // If player score < computer score. Show Message Player Lose
 // Else Show Message The Result is equal
-function playGame() {
-    // Create variables to keep track human's score and computer's score
-    let playerScore = 0;
-    let computerScore = 0;
-    // -- Start Function playRound
-    //Get human choice and computer choice
-    // If human choice === computer choice so the result is equal
-    // End If
-    // Else 
-    // If human choice is Rock, check computer choice is paper/scissor
-    // If computer choice is paper then result is Player Lose, computerScore++
-    // If computer choice is scissor then result is Player Win, playerScore++
-    // Another human/computer choices do the same
-    // End Else
-    // Go to the next round
-    // -- End Function playRound
-    let playRound = function (playerChoice, computerChoice) {
-        if (playerChoice === computerChoice) {
-            console.log("The result is equal!");
-        }
-        else {
-            if (playerChoice === "Rock") {
-                if (computerChoice === "Paper") {
-                    console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
-                    computerScore++;
-                }
-                else {
-                    console.log(`You Win! ${playerChoice} beats ${computerChoice}`);
-                    playerScore++;
-                }
-            }
-
-            else if (playerChoice === "Paper") {
-                if (computerChoice === "Scissor") {
-                    console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
-                    computerScore++;
-                }
-                else {
-                    console.log(`You Win! ${playerChoice} beats ${computerChoice}`);
-                    playerScore++;
-                }
-            }
-
-            else {
-                if (computerChoice === "Rock") {
-                    console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
-                    computerScore++;
-                }
-                else {
-                    console.log(`You Win! ${playerChoice} beats ${computerChoice}`);
-                    playerScore++;
-                }
-            }
-        }
-    }
-
+function checkWhoWin() {
+    /*
     for(let round = 1; round <= 5; round++) {
         playRound(getPlayerChoice(), getComputerChoice());
         console.log(`Round: ${round}.
             Player Score: ${playerScore}.
             Computer Score: ${computerScore}`);
     }
+    */
 
-    if (playerScore > computerScore) {
-        console.log("Player Win!");
+    if (playerScore === winScore) {
+        updateResult("Game Over. Player Win!");
     }
 
-    else if (playerScore < computerScore) {
-        console.log("Computer Win!");
+    else if (computerScore === winScore) {
+        console.log("Game Over. Computer Win!");
     }
-    else {
-        console.log("The result is equal!");
+
+}
+
+function updateResult(result) {
+    paraResult.textContent = result;
+    paraPlayerScore.textContent = `Player Score: ${playerScore}`;
+    paraComputerScore.textContent = `Computer Score: ${computerScore}`;
+
+    if (playerScore === winScore) {
+        paraResult.textContent = "Game Over. Player Win!";
+        isGameOver = true;
+    }
+
+    else if (computerScore === winScore) {
+        paraResult.textContent = "Game Over. Computer Win!";
+        isGameOver = true;
     }
 }
 
-playGame();
+// Event
+let rockButton = document.querySelector("#rock-btn");
+let paperButton = document.querySelector("#paper-btn");
+let scissorButton = document.querySelector("#scissor-btn");
+
+let paraResult = document.querySelector("#result");
+let paraPlayerScore = document.querySelector("#player-score");
+let playerChoiceImg = document.querySelector("#player-choice-img");
+let paraComputerScore = document.querySelector("#computer-score");
+let computerChoiceImg = document.querySelector("#computer-choice-img");
+
+let winScore = 5;
+let isGameOver = false;
+
+rockButton.addEventListener("click", () => {
+    if (isGameOver === false) {
+        displayPlayerChoice("Rock");
+        playRound("Rock", getComputerChoice())
+    }
+    else {
+        alert("The game is over!");
+    }
+});
+
+paperButton.addEventListener("click", () => {
+    if (isGameOver === false) {
+        displayPlayerChoice("Paper");
+        playRound("Paper", getComputerChoice())
+    }
+    else {
+        alert("The game is over!");
+    }
+});
+
+scissorButton.addEventListener("click", () => {
+    if (isGameOver === false) {
+        displayPlayerChoice("Scissor");
+        playRound("Scissor", getComputerChoice())
+    }
+    else {
+        alert("The game is over!");
+    }
+});
+
+//playGame();
